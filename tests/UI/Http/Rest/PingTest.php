67cae72d9d56c6aedbf71e6ef1c539c2a998b2aa
Alexander Hutorezki <huto@wa-gruppe.com>
2020-09-26 00:01:02 +0200

    chore: Create Webapplication Codebase

---
+++ tests/UI/Http/Rest/PingTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Waglpz\Webapp\Tests\UI\Http\Rest;
+
+use Waglpz\Webapp\Tests\UI\RestTestCase;
+
+final class PingTest extends RestTestCase
+{
+    /** @test */
+    public function pingDerGesundheit(): void
+    {
+        $uri      = '/api/ping';
+        $response = $this->restGetResponse($uri);
+        self::assertSame(200, $response->getStatusCode());
+        $json = (string) $response->getBody();
+        $data = \json_decode($json, true, 512, \JSON_THROW_ON_ERROR);
+        self::assertArrayHasKey('time', $data);
+        self::assertArrayHasKey('apiVersion', $data);
+    }
+}
