7546918efda515fb4a336decefb27a5e97fd0e35
Alexander Hutorezki <huto@wa-gruppe.com>
2021-08-03 17:41:08 +0200

    chore: Improve / Extend config

--- config/main.php
+++ config/main.php
@@ -6,8 +6,14 @@ declare(strict_types=1);
 
 return [
     'apiVersion'          => '0.1.0',
-    'logErrorsDir'          => '/tmp',
+    'logErrorsDir'        => '/tmp',
     'router'              => include 'router.php',
+    'db'                  => [
+        'dsn' => 'localhost',
+        'username' => 'username',
+        'password' => 'password',
+    ],
+    'logger'              => include 'logger.php',
     'view'                => [
         'templates'  => \dirname(__DIR__) . '/templates/',
         'attributes' => ['webseitenTitle' => 'Testseite'],
